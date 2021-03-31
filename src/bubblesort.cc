72aa0481e707599b86b5e8cf3bf5255aafdcd72e
FudgeRacoon <fudgeracoon@gmail.com>
2021-03-31 16:29:32 +0200

    Minor tweaks

--- src/bubblesort.cc
+++ src/bubblesort.cc
@@ -3,63 +3,47 @@ using namespace std;
 
 class BubbleSort
 {
-	public:
-		static void Sort(int *a, int num)
-		{
-			bool swap;
-			do
-			{
-				swap = false;
-				int i = 0;
-				while(i<num-1)
-				{
-					if(a[i] > a[i+1])
-					{
-						Swap(&a[i], &a[i+1]);
-						swap = true;
-					}
-					i++;
-						
-				}
-			}while(swap);
-		}
+public:
+	static void Swap(int *a, int *b)
+	{
+		int temp = *a;
+		*a = *b;
+		*b = temp;
+	}
 
-		static void Swap(int *a, int *b)
-		{
-			if(*a!=*b)
-			{
-				*a ^= *b;
-				*b ^= *a;
-				*a ^= *b;
-			}
-			
-		}
-
+	static void Sort(int *array, int size)
+	{
+		for(int pass = 0; pass < size - 1; pass++)
+			for(int index = 0; index < size - pass - 1; index++)
+				if(array[index] > array[index + 1])
+					Swap(&array[index], &array[index + 1]);
+		
+	}
 };
 
 int main()
 {
-	int num,i=0;
-	cout<<"No. of elements:";
-	cin>>num;
+	int size ;
+
+	cout << "Enter size of array: ";
+	cin >> size;
 	
-	int a[num];
+	int array[size];
 
-	while(i<num)
+	cout << endl << "Enter the elements:" << endl;
+	for(int i = 0; i < size; i++)
 	{
-		cout<<"Value for a["<<i<<"]:";
-		cin>>a[i++];		
-	}		
-
-	BubbleSort::Sort(a,num);
-
-	cout<<"\n\nSorted Array:";
-	i=0;
-	while(i<num)
-	{
-		cout<<a[i]<< ", ";
-		i++;	
+		cout << "\telement " << i << " >> ";
+		cin >> array[i];
 	}
-	
-	
+
+	cout << "Array before sorting:" << endl;
+	for(int i = 0; i < size; i++)
+		cout << "\telement " << i << " >> " << array[i] << endl;
+
+	BubbleSort::Sort(array, size);
+
+	cout << "Array after sorting:" << endl;
+	for(int i = 0; i < size; i++)
+		cout << "\telement " << i << " >> " << array[i] << endl;
 }
